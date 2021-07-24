0e101c2cb3d6f01034d2916a1026ea66331517cd
Chris Chrome <christophercookman@gmail.com>
2021-07-24 08:28:55 -0400

    It kinda works now
    Needs more string sanitation before being put into use

--- index.js
+++ index.js
@@ -10,3 +10,32 @@ const express = require('express');
 const Discord = require("discord.js");
 const config  = require("./config.json")
 const app = express();
+hook_creds = config.discord.webhook_url.split("/webhooks/")[1].split("/")
+const hook = new Discord.WebhookClient(hook_creds[0], hook_creds[1]);
+
+// Little functions i need
+function escapeMarkdown(text) {
+    var unescaped = text.replace(/\\(\*|_|`|~|\\)/g, '$1'); // unescape any "backslashed" character
+    var escaped = unescaped.replace(/(\*|_|`|~|\\)/g, '\\$1'); // escape *, _, `, ~, \
+    return escaped;
+}
+
+app.get('/sendmsg', (req, res) => {
+    res.set("Connection", "close")
+    if(!req.query.name || !req.query.msg) {
+        res.sendStatus(400)
+        res.end();
+    }
+    // Needs more string sanitation lol
+    hook.send(escapeMarkdown(req.query.msg), {username: escapeMarkdown(req.query.name)}).then(() => {
+        res.sendStatus(200);
+        res.end();
+    }).catch((err) => {
+        res.sendStatus(500);
+        res.end();
+    })
+});
+
+app.listen(config.stormworks.listen_port, "127.0.0.1", () => {
+    console.log('server started');
+  });
